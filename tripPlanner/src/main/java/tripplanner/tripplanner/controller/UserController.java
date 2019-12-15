ab8ffd0db2c64566e2d7de983a1db4bbe25b066f
yuanchen <yuanchenshi@gmail.com>
2019-12-15 17:42:51 -0600

    uncomment add files

--- tripPlanner/src/main/java/tripplanner/tripplanner/controller/UserController.java
+++ tripPlanner/src/main/java/tripplanner/tripplanner/controller/UserController.java
@@ -1,32 +1,28 @@
-package tripplanner.tripplanner.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import tripplanner.tripplanner.model.User;
-import tripplanner.tripplanner.service.UserService;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:3000")
-public class UserController {
-  @Autowired private UserService userService;
-
-  @GetMapping("/register/{username}/{password}")
-  public ResponseEntity<HttpStatus> Register(
-      @PathVariable String username, @PathVariable String password) {
-    User newUser = new User();
-    userService.addUser(newUser);
-    return ResponseEntity.ok(HttpStatus.OK);
-  }
-
-  //    @PostMapping("/register")
-  //    public ResponseEntity<HttpStatus> Register(@RequestBody LoginForm loginForm) {
-  //      userService.addUser(loginForm);
-  //      return ResponseEntity.ok(HttpStatus.OK);
-  //    }
-}
-
+//package tripplanner.tripplanner.controller;
+//
+////@RestController
+////@CrossOrigin(origins = "http://localhost:3000")
+////public class UserController {
+////  @Autowired private UserService userService;
+////
+////  @GetMapping("/register/{username}/{password}")
+////  public ResponseEntity<HttpStatus> Register(
+////      @PathVariable String username, @PathVariable String password) {
+////    User newUser = new User();
+////    userService.addUser(newUser);
+////    return ResponseEntity.ok(HttpStatus.OK);
+////  }
+////
+////  //    @PostMapping("/register")
+////  //    public ResponseEntity<HttpStatus> Register(@RequestBody LoginForm loginForm) {
+////  //      userService.addUser(loginForm);
+////  //      return ResponseEntity.ok(HttpStatus.OK);
+////  //    }
+////}
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@RequestMapping(value = "/user/registration", method = RequestMethod.GET)
+//public String showRegistrationForm()
+//
