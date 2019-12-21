2f4c45e833edb6bf43347ec357097e37af86fe78
bingzhoucmu <vincentzhoubing@gmail.com>
2019-12-21 02:24:04 -0500

    replace JPA to CRUD

--- tripPlanner/src/main/java/tripplanner/tripplanner/dao/ItemDao.java
+++ tripPlanner/src/main/java/tripplanner/tripplanner/dao/ItemDao.java
@@ -1,7 +1,9 @@
 package tripplanner.tripplanner.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import tripplanner.tripplanner.model.Item;
 
-public interface ItemDao extends JpaRepository<Item, String>  {}
+@Repository
+public interface ItemDao extends CrudRepository<Item, String> { }
