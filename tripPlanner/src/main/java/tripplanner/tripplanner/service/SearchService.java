818f58de3b044ffba95ab62e51f83a05b1801854
bingzhoucmu <vincentzhoubing@gmail.com>
2019-12-21 02:33:42 -0500

    moved externalClient to service

--- tripPlanner/src/main/java/tripplanner/tripplanner/service/SearchService.java
+++ tripPlanner/src/main/java/tripplanner/tripplanner/service/SearchService.java
@@ -5,8 +5,9 @@ import tripplanner.tripplanner.model.Item;
 import java.util.List;
 
 public interface SearchService {
-
-    //	search events from a given geolocation (probably type of event)
-    Item searchEvent(double lat, double lon);
+    // for landing
+    List<Item> searchEvents();
+    // for keyword search
+    List<Item> searchEvents(String keyword);
 
 }
