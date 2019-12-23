0001bdfd3ec3f77b7f1a9d6c4cf0782097885e13
yuanchen <yuanchenshi@gmail.com>
2019-12-22 18:38:39 -0600

    modify userController.java

--- tripPlanner/src/main/java/tripplanner/tripplanner/model/User.java
+++ tripPlanner/src/main/java/tripplanner/tripplanner/model/User.java
@@ -81,13 +81,4 @@ public class User implements Serializable {
   public void setCores_profile(Profile cores_profile) {
     this.cores_profile = cores_profile;
   }
-
-  private int active;
-
-  @ManyToMany(cascade = CascadeType.ALL)
-  @JoinTable(
-      name = "user_role",
-      joinColumns = @JoinColumn(name = "user_id"),
-      inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private Set<Role> roles;
 }
