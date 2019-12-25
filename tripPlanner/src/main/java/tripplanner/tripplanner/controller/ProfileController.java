ba5e930c27e222b5b67c01415ade9297bfa21feb
oliveydeng@gmail.com <29358427+oliveDY@users.noreply.github.com>
2019-12-25 14:38:01 -0500

    add crossorigin annotation

--- tripPlanner/src/main/java/tripplanner/tripplanner/controller/ProfileController.java
+++ tripPlanner/src/main/java/tripplanner/tripplanner/controller/ProfileController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import tripplanner.tripplanner.service.ProfileService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProfileController {
 	
 	@Autowired
